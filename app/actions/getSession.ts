10f01772bf4282a817147bf3d896589477b1f823
Ziyodullayev Dilshodbek <ziyodullayevdilshod7@gmail.com>
2024-09-18 20:15:44 +0500

    fix auth error

--- app/actions/getSession.ts
+++ app/actions/getSession.ts
@@ -1,5 +1,4 @@
-import { authOptions } from "../api/auth/[...nextauth]/route";
-
+import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
 
 export default async function getSession() {
